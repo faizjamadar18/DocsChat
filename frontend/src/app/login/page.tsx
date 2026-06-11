c9c632c2d98b1495aa1fc52c688be4ecfd9116f7
faizjamadar18 <faizjamadar18@gmail.com>
2026-06-11 19:23:48 +0530

    .

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -32,7 +32,7 @@ function LoginForm() {
 
   return (
     <div className="flex h-screen items-center justify-center bg-[#000000]">
-      <div className="bg-surface p-8 sm:p-12 rounded-2xl w-full max-w-md border border-white/10 shadow-lg mx-4">
+      <div className="p-8 sm:p-12 rounded-2xl w-full max-w-md shadow-lg mx-4">
         <div className="text-center mb-8">
           <h1 className="text-2xl font-semibold mb-2 text-white">Welcome Back</h1>
           <p className="text-white/60 text-sm">Sign in to continue to your notebook</p>
@@ -46,24 +46,25 @@ function LoginForm() {
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-5">
           <div>
-            <label className="block mb-2 text-sm text-white/60">Email</label>
+            {/* <label className="block mb-2 text-sm text-white/60">Email</label> */}
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full px-4 py-3 rounded-lg bg-white/5 border border-white/10 text-white outline-none focus:outline-none placeholder:text-white/30"
+              placeholder='Enter your email address...'
+              className="w-full px-4 py-2 rounded-lg bg-white/5 border border-white/10 text-white outline-none focus:outline-none placeholder:text-white/30 text-sm"
             />
           </div>
 
           <div>
-            <label className="block mb-2 text-sm text-white/60">Password</label>
+            {/* <label className="block mb-2 text-sm text-white/60">Password</label> */}
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="w-full px-4 py-3 rounded-lg bg-white/5 border border-white/10 text-white outline-none focus:outline-none placeholder:text-white/30"
+              placeholder='Enter password'
+              className="w-full px-4 py-2 rounded-lg bg-white/5 border border-white/10 text-white outline-none focus:outline-none placeholder:text-white/30 text-sm"
             />
           </div>
 
@@ -72,7 +73,7 @@ function LoginForm() {
           <button
             type="submit"
             disabled={loading}
-            className="bg-white text-base py-[14px] rounded-lg font-semibold mt-2 disabled:opacity-70 cursor-pointer"
+            className="bg-surface text-white py-[10px] rounded-4xl font-semibold mt-2 disabled:opacity-70 cursor-pointer"
           >
             {loading ? 'Signing in...' : 'Sign In'}
           </button>
